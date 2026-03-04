1af2f6fc3bdd895195ce7347ee63e28767bad1fa
LucianaVC752 <luciana.velasquez752@pascualbravo.edu.co>
2026-03-04 16:13:16 -0500

    Primera actualizacion con Lambdas y streams

---
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -0,0 +1,109 @@
+package com.aluracursos.screenmatch.principal;
+
+import com.aluracursos.screenmatch.model.DatosEpisodio;
+import com.aluracursos.screenmatch.model.DatosSerie;
+import com.aluracursos.screenmatch.model.DatosTemporada;
+import com.aluracursos.screenmatch.model.Episodio;
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+import com.aluracursos.screenmatch.service.ConvierteDatos;
+import org.springframework.cglib.core.Local;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Principal {
+    private Scanner teclado = new Scanner(System.in);
+    private ConsumoAPI consumoApi = new ConsumoAPI();
+    private final String URL_BASE = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=4fc7c187";
+    private ConvierteDatos conversor = new ConvierteDatos();
+    public void muestraElMenu(){
+        System.out.println("Escribe el nombre de la série que deseas buscar");
+        //Busca los datos generales de las series
+        var nombreSerie = teclado.nextLine();
+        var json = consumoApi.obtenerDatos(URL_BASE + nombreSerie.replace(" ", "+") + API_KEY);
+        //https://www.omdbapi.com/?t=game+of+thrones&apikey=4fc7c187
+        DatosSerie datos = conversor.obtenerDatos(json, DatosSerie.class);
+        System.out.println(datos);
+
+        //Busca los datos de todas las temporadas
+        List<DatosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i <= datos.totalTemporadas(); i++) {
+            json = consumoApi.obtenerDatos(URL_BASE + nombreSerie.replace(" ", "+") + "&Season=" + i + API_KEY);
+            DatosTemporada datosTemporada = conversor.obtenerDatos(json, DatosTemporada.class);
+            temporadas.add(datosTemporada);
+        }
+        temporadas.forEach(System.out::println);
+
+        //Mostrar solo el titulo de los episodios para las temporadas
+        for (int i = 0; i < datos.totalTemporadas(); i++) {
+            List<DatosEpisodio> episodiosTemporadas = temporadas.get(i).episodios();
+            for (int j = 0; j < episodiosTemporadas.size(); j++) {
+                System.out.println(episodiosTemporadas.get(j).titulo());
+            }
+        }
+        // Mejoría usando funciones Lambda
+        //temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+        //Convertir la info a una lista de Datos Episodios
+
+        List<DatosEpisodio> datosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+        // top 5 episodios
+        System.out.println("Top 5 episodios");
+        datosEpisodios.stream()
+                .filter(e -> !e.evaluacion().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DatosEpisodio::evaluacion).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+
+        //Convirtiendo los datos a una lista del tipo Episodio
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                .map(d -> new Episodio(t.numero(), d)))
+                .collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+        //Busqueda de episios a apartir de x año
+        System.out.println("Por favor indica el año a partir del cual deseas var los episodios");
+        var fecha = teclado.nextLine();
+
+        LocalDate fechaBusqueda = LocalDate.of(Integer.valueOf(fecha),1,1);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        episodios.stream()
+                .filter(e ->e.getFechaDeLanzamiento() != null && e.getFechaDeLanzamiento().isAfter(fechaBusqueda))
+                .forEach(e -> System.out.println(
+                    "Temporada " + e.getTemporada() +
+                            "Episodio" + e.getTitulo() +
+                            "Fecha de Lanzamiento" + e.getFechaDeLanzamiento().format(dtf)
+
+                ));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
